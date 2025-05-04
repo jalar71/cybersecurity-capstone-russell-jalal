1474fabfc80c64f2c38bc2903e302d821c0dc7af
jalar71 <russellanthonyjalal@outlook.com>
2025-05-04 13:29:22 -0400

    Delete powershell directory

--- powershell/export_users.ps1
+++
@@ -1,1 +0,0 @@
-Get-ADUser -Filter * -SearchBase "OU=Employee,DC=yourdomain,DC=com" | Select-Object Name, SamAccountName, UserPrincipalName | Export-Csv -Path "C:\path\to\your\outputfile.txt" -NoTypeInformation
